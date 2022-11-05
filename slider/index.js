38d686f0f83ee05c7a03d9cb813fd7be8b88dff6
feroswin <vitalkaivanov009@mail.ru>
2022-11-05 23:02:07 +0300

    slider update

--- slider/index.js
+++ slider/index.js
@@ -2,31 +2,40 @@ const arrowLeft = document.querySelector(".arrow-left");
 const arrowRight = document.querySelector(".arrow-right");
 const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slide").length;
-const circles = document.querySelectorAll(".slider-circle");
+const circles = document.querySelectorAll(".slider-menu-item");
 
 let slideNumber = 0
 
+function firstSlide() {
+  slideNumber = 0
+  slider.style.transform = `translateX(${slideNumber}px)`
+}
+
+function lastSlide() {
+  slider.style.transform = `translateX(-${(slides-1) * 800}px)`
+  slideNumber = slides-1
+}
+
+function nextSlide() {
+  slider.style.transform = `translateX(-${slideNumber * 800}px)`
+}
+
+function prevSlide() {
+  slider.style.transform = `translateX(-${(slideNumber * 800)}px)`
+}
+
+
 arrowRight.addEventListener("click", () => {
-  slideNumber++
-  if (slideNumber >= slides) {
-    slideNumber = 0
-    slider.style.transform = `translateX(${slideNumber}px)`
-  }
-  else {
-    slider.style.transform = `translateX(-${slideNumber * 800}px)`
-  }
-  console.log('right ', slideNumber)
+  slideNumber++;
+  (slideNumber >= slides) ? firstSlide() : nextSlide();
+  console.log('right ',slideNumber)
 })
 
 arrowLeft.addEventListener("click", () => {
-  slideNumber--
-  if (slideNumber < 0) {
-    slider.style.transform = `translateX(-${(slides-1) * 800}px)`
-    slideNumber = slides-1
-  }
-  else {
-    slider.style.transform = `translateX(-${(slideNumber * 800)}px)`
-  }
-  console.log('left ', slideNumber)
+  slideNumber--;
+  (slideNumber < 0) ? lastSlide() : prevSlide();
+  console.log('left ',slideNumber)
 })
+
+
 
